80fe8fec8058ba82757bfb0e056a3ba6a319f032
Pitypalatty <pitypalattysama@gmail.com>
2016-11-27 20:55:54 +0100

    build the dist folder on the server

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -1,13 +1,13 @@
 // This script copies src/index.html into /dist/index.html
 // This is a good example of using Node and cheerio to do a simple file transformation.
 // In this case, the transformation is useful since we only use a separate css file in prod.
-import fs from 'fs';
-import cheerio from 'cheerio';
-import colors from 'colors';
+var fs =  require('fs');
+var cheerio = require('cheerio');
+var colors = require('colors');
 
 /*eslint-disable no-console */
 
-fs.readFile('src/index.html', 'utf8', (err, markup) => {
+fs.readFile('src/index.html', 'utf8', function(err, markup){
   if (err) {
     return console.log(err);
   }
