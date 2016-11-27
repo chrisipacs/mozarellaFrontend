b82438796c821b86e6cefcc7f31070b29f42d015
Pitypalatty <pitypalattysama@gmail.com>
2016-11-27 20:30:25 +0100

    start distServer as default

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import path from 'path';
-import open from 'open';
-import compression from 'compression';
+var express =require('express');
+var path = require('path');
+var open = require('open');
+var compression = require('compression');
 
 /*eslint-disable no-console */
 
-const port = $PORT;
+
+const port = process.env.$PORT || 3000;
 const app = express();
 
 app.use(compression());
