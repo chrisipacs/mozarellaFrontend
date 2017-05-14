6372678e63e6a8b79cd5b8d3232ffa96424686a3
Pitypalatty <pitypalattysama@gmail.com>
2017-05-14 21:30:52 +0200

    correctly initialize students list

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -8,7 +8,8 @@ export default {
         listsContext: { //the lists the student is associated with
             totalCount: 0,
             pageSize: 10,
-            activePage:0
+            activePage:0,
+            lists: []
         }
     },
     listsContext: { //ALL lists that are shown to the user when looking for courses to subscribe to
